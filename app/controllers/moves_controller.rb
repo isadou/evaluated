8e33482d3442ceaeb28c16f1b5f5f8e5bd0feb72
Lilia Bekrar <liliarossi2504@gmail.com>
2022-01-07 11:09:55 +0100

    change count rooms and add count stuffs

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -52,10 +52,10 @@ class MovesController < ApplicationController
     params["roomscreation"].each do |room_type, number|
       number = number.to_i
       unless number.zero?
-        i = 0
+        i = 1
         number.times do
           name = room_type
-          name += " #{i}" unless i == 0
+          # name += " #{i}" unless i == 0
           Room.create!(name: name, move_id: @move.id, room_type_id: RoomType.find_by(name: room_type).id)
           i += 1
         end
