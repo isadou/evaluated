7be6c6f8b0952efb2b9e217e95bd2d785023650d
Lilia Bekrar <liliarossi2504@gmail.com>
2022-01-11 11:18:12 +0100

    functions plus and minus ok

--- app/javascript/plugins/init_plus_minus_buttons.js
+++ app/javascript/plugins/init_plus_minus_buttons.js
@@ -1,12 +1,47 @@
 const initPlusMinusButtons = () => {
-const buttonPlus = document.querySelector('.plus');
+const buttonPlus = document.querySelectorAll('.plus');
+const buttonMoinsRooms = document.querySelectorAll('.moins-rooms');
+const buttonMoinsStuffs = document.querySelectorAll('.moins-stuff')
+let i = 0;
 
-  buttonPlus.addEventListener('click', (e) => {
+  buttonPlus.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
     e.preventDefault();
     let divPlus = e.target.parentElement.parentElement;
     let input = divPlus.previousElementSibling.firstElementChild.firstElementChild;
     input.value = parseInt(input.value, 10) + 1;
+    i += 1;
+    console.log(i);
+    });
   });
+
+  buttonMoinsRooms.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      let divMoins = e.target.parentElement.parentElement;
+      let input = divMoins.nextElementSibling.firstElementChild.firstElementChild;
+      if (i > 0) {
+        input.value = parseInt(input.value, 10) - 1;
+        i -= 1;
+        console.log(i);
+      }
+    });
+  });
+
+  buttonMoinsStuffs.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      let divMoins = e.target.parentElement.parentElement;
+      let input = divMoins.nextElementSibling.firstElementChild.firstElementChild;
+      if (i > 0) {
+        input.value = parseInt(input.value, 10) - 1;
+        i -= 1;
+        console.log(i);
+      }
+    });
+  });
+
+
 }
 
 export { initPlusMinusButtons };
